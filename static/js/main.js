86b94dc51f324dafce9b5a6e656da239d854fe22
Bro223 <Aleks.bratsu@gmail.com>
2018-02-11 10:49:33 +0200

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,14 @@
+;
+// Начинать писать отсюда!!!!
+
+$(document).ready(function() {
+
+})
+
+$(document).ready(function() {
+	$('.js-top-banner').bxSlider({
+			controls:false,
+			captions: true,
+			pagerCustom: '.top-banner__pag',
+		});	
+});
